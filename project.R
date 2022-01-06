71c42743d07adeb586f2a044314ee512c53e6f12
AR Sai Prasath <79992803+ARSaiPrasath@users.noreply.github.com>
2022-01-06 11:32:48 +0530

    Update project.R

--- project.R
+++ project.R
@@ -1,7 +1,8 @@
 #loading libraries 
 library(data.table) 
 library(dplyr)     
-library(ggplot2)     
+library(ggplot2)  
+library(cowplot)
 
 train = fread("Train_UWu5bXk.csv")
 test = fread("Test_u94Q5KV.csv")
@@ -18,6 +19,18 @@ test[,Item_Outlet_Sales := NA]
 # combining train and test data sets
 combi = rbind(train, test) 
 
+ggplot(train) + geom_histogram(aes(train$Item_Outlet_Sales), binwidth = 100, fill = "orange") +
+  xlab("Item_Outlet_Sales")
+
+
+p1 = ggplot(combi) + geom_histogram(aes(Item_Weight), binwidth = 0.5, fill = "blue")
+
+p2 = ggplot(combi) + geom_histogram(aes(Item_Visibility), binwidth = 0.005, fill = "blue")
+
+p3 = ggplot(combi) + geom_histogram(aes(Item_MRP), binwidth = 1, fill = "blue")
+
+plot_grid(p1, p2, p3, nrow = 1) # plot_grid() from cowplot package
+
 ggplot(combi %>% group_by(Item_Fat_Content) %>% summarise(Count = n())) + 
   geom_bar(aes(Item_Fat_Content, Count), stat = "identity", fill = "red")
 
@@ -28,6 +41,15 @@ combi$Item_Fat_Content[combi$Item_Fat_Content == "reg"] = "Regular"
 ggplot(combi %>% group_by(Item_Fat_Content) %>% summarise(Count = n())) + 
   geom_bar(aes(Item_Fat_Content, Count), stat = "identity", fill = "blue")
 
+# plot for Item_Type
+p4 = ggplot(combi %>% group_by(Item_Type) %>% summarise(Count = n())) + 
+  geom_bar(aes(Item_Type, Count), stat = "identity", fill = "coral1") +
+  xlab("") +
+  geom_label(aes(Item_Type, Count, label = Count), vjust = 0.5) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  ggtitle("Item_Type")
+
+p4
 ## Missing Value Treatment
 missing_index = which(is.na(combi$Item_Weight))
 for(i in missing_index){
@@ -36,7 +58,6 @@ for(i in missing_index){
   combi$Item_Weight[i] = mean(combi$Item_Weight[combi$Item_Identifier == item], na.rm = T)
   
 }
-
 # replacing 0 in Item_Visibility with mean
 zero_index = which(combi$Item_Visibility == 0)
 for(i in zero_index){
@@ -45,8 +66,6 @@ for(i in zero_index){
   combi$Item_Visibility[i] = mean(combi$Item_Visibility[combi$Item_Identifier == item], na.rm = T)
   
 }
-
-
 ## Feature Engineering
 perishable = c("Breads", "Breakfast", "Dairy", "Fruits and Vegetables", "Meat", "Seafood")
 non_perishable = c("Baking Goods", "Canned", "Frozen Foods", "Hard Drinks", "Health and Hygiene",
